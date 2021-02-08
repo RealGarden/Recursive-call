817918af8da5e9a455a9536f190d9b370f449162
sudhdkso <78727847+sudhdkso@users.noreply.github.com>
2021-02-08 19:41:19 +0900

    Update 소스.cpp

--- 소스.cpp
+++ 소스.cpp
@@ -4,6 +4,6 @@ asoidjfisodjfoasjdoifjasodjfoasjdofisjiodf
 int main() {
 	cout << "hello jiwon World~~~" << endl;
 	cout << "nannannannananana~~" << endl;
-
+	cout<<"지금은 다이렉트입니다"<<endl;
 	return 0;
 }
