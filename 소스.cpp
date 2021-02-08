c6eb251d52bf79315c10e9aa549558c0120704e7
jjjGi <78727864+jjjGi@users.noreply.github.com>
2021-02-08 19:41:31 +0900

    Create 소스.cpp
    충돌과연?~?~?~?~?~?~

--- 소스.cpp
+++ 소스.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 asoidjfisodjfoasjdoifjasodjfoasjdofisjiodf
 int main() {
+	cout<<"충돌~!~!~!~!~!~!"<<endl;
 	cout << "hello jiwon World~~~" << endl;
 	cout << "nannannannananana~~" << endl;
-	cout<<"지금은 다이렉트입니다"<<endl;
+
 	return 0;
 }
